56fcf2a3365a50a4f76f6462eada65f6a848cba3
Hemant Rai <hemantrai1988@gmail.com>
2013-12-18 19:43:41 +0530

    The filter script
    This function can be used to filter any datatables column with date values. You can select the range by using two date-pickers to get the min and max values. You might have to change the function a bit to make it work for different date formats. By default, the function will filter the third datatables column(index=2)which has date in format mm/dd/yyyy

---
+++ daterangefilter.js
@@ -0,0 +1,40 @@
+			$.fn.dataTableExt.afnFiltering.push(
+				function( oSettings, aData, iDataIndex ) {
+					if ($('#min').val() != '' || $('#max').val() != '') {
+					var iMin_temp = $('#min').val();
+					if (iMin_temp == '') {
+					  iMin_temp = '01/01/1980';
+					}
+					var iMax_temp = $('#max').val();
+					if (iMax_temp == '') {
+					  iMin_temp = '01/01/2014';
+					}
+					
+					var arr_min = iMin_temp.split("/");
+					var arr_max = iMax_temp.split("/");
+					var arr_date = aData[2].split("/");
+          
+          var iMin = new Date(arr_min[2], arr_min[0], arr_min[1], 0, 0, 0, 0)
+					var iMax = new Date(arr_max[2], arr_max[0], arr_max[1], 0, 0, 0, 0)
+					var iDate = new Date(arr_date[2], arr_date[0], arr_date[1], 0, 0, 0, 0)
+					
+					if ( iMin == "" && iMax == "" )
+					{
+						return true;
+					}
+					else if ( iMin == "" && iDate < iMax )
+					{
+						return true;
+					}
+					else if ( iMin <= iDate && "" == iMax )
+					{
+						return true;
+					}
+					else if ( iMin <= iDate && iDate <= iMax )
+					{
+						return true;
+					}
+					return false;
+					}
+				}
+			);
